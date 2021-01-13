0d70c9c75578fa247f42002ba81375767375e3e8
jane baik <janebaik@berkeley.edu>
2021-01-13 13:19:12 -0800

    --

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :login?
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session:[session_token])
+    end
+
+    def ensure_login
+    end
+
+    def login?
+    end
+
+    def login!
+    end
+
+    def logout!
+    end
 end
